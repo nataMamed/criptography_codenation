917b4c98c39f7e8f2cb32267a6c5f40c180531d1
Natã <natamamede@gmail.com>
2020-04-30 14:36:49 -0300

    "Alterando funcionalidades de acces-api e adicionando check_connection"

--- message.py
+++ message.py
@@ -5,18 +5,27 @@ class Message:
     from the API, it will get the message and it will
     have methods to get specifcs informations """
 
-    def __init__(self):
-        pass
+    def __init__(self, token):
+        """Initialize the attributes of the message"""
 
 
     def access_api(self, token):
-        """This fuction will accessthe api returnning the json"""
+        """This fuction will accessthe api returnning the reponse"""
 
         default_url = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data'
         params ={'token': str(token)}
-
         response = requests.get(default_url, params=params)
 
-        json = response.json()
-        return json
+        return response
+
+    @staticmethod
+    def check_connection(response):
+        """Checks if the connections was well succeed"""
+
+        if response:
+            return True 
+        else:
+            raise ValueError('Invalid Token!!!')
+
+
 
