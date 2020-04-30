cc548006d34bc9d7f3f5e5eec8e4a5dfd18d55ea
Natã <natamamede@gmail.com>
2020-04-30 16:45:12 -0300

    "Primeria etapa concluida, conseguir acessar a api"

---
+++ main.py
@@ -0,0 +1,8 @@
+import requests
+from message import Message
+
+my_token  = '8861368f950c19d5a1af3239cf4158cbe840ae77'
+message = Message(my_token)
+print(message)
+
+
--- message.py
+++ message.py
@@ -8,24 +8,46 @@ class Message:
     def __init__(self, token):
         """Initialize the attributes of the message"""
 
+        token = str(token)
 
-    def access_api(self, token):
+        if self.check_connection(token):
+            self._json = self.access_api(token).json()
+        else:
+            raise ValueError('Something went wrong')
+      
+    def __str__(self):
+        """Represents the class like a string"""
+
+        return self.format_json(self._json)
+
+    @staticmethod
+    def format_json(json):
+        """Pretify the received json"""
+
+        dictionary_json = json
+        return """Cifrado: {}\n
+        Decifrado: {}""". format(dictionary_json['cifrado'], dictionary_json['decifrado'])
+                                
+
+    @staticmethod
+    def access_api(token):
         """This fuction will accessthe api returnning the reponse"""
 
         default_url = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data'
-        params ={'token': str(token)}
+        params ={'token': token}
         response = requests.get(default_url, params=params)
 
         return response
 
-    @staticmethod
-    def check_connection(response):
+    
+    def check_connection(self, token):
         """Checks if the connections was well succeed"""
 
+        response = self.access_api(token)
         if response:
             return True 
         else:
-            raise ValueError('Invalid Token!!!')
+            raise ValueError('Invalid Token or connection!!!')
 
 
 
